965ceaa4676f536bf684d0d540d97e351f787270
hyunhwa <binooooo@naver.com>
2020-12-29 16:05:18 +0900

    posts 테이블 title 컬럼 unique 속성 삭제

--- api/models/Post.go
+++ api/models/Post.go
@@ -12,7 +12,7 @@ import (
 /* post 구조체 */
 type Post struct {
 	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	Title     string    `gorm:"size:255;not null;unique" json:"title"`
+	Title     string    `gorm:"size:255;not null;" json:"title"`
 	Content   string    `gorm:"size:255;not null;" json:"content"`
 	Author    User      `json:"author"`
 	AuthorID  uint32    `gorm:"not null" json:"author_id"`
